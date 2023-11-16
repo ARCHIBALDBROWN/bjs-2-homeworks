23b31e0b4adc79bb9c2022e5cf06a8ad13a673e6
Archibaldbrown <bizzy_boss@mail.ru>
2023-11-16 20:13:44 +0100

    Решение задаче 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,35 +1,51 @@
-"use strict"
+"use strict";
 
+// Задача 1
 function solveEquation(a, b, c) {
   let discriminant = b ** 2 - 4 * a * c;
-  let result = [];
+  let roots = [];
 
-  if (discriminant > 0) {
+  if (discriminant < 0) {
+    // нет корней
+    return roots;
+  } else if (discriminant === 0) {
+    // один корень
+    let root = -b / (2 * a);
+    roots.push(root);
+  } else {
+    // два корня
     let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
     let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    result = [root1, root2];
-  } else if (discriminant === 0) {
-    let root = -b / (2 * a);
-    result = [root];
+    roots.push(root1, root2);
   }
 
-  return result;
+  return roots;
 }
 
+// Задача 2
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
     return "Введите корректные числовые значения";
   }
 
-  let loanBody = amount - contribution;
-  let monthlyPercent = percent / 100 / 12;
-  let monthlyPayment =
+  const monthlyPercent = percent / 100 / 12;
+  const loanBody = amount - contribution;
+  const monthlyPayment =
     loanBody *
     (monthlyPercent + monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1));
-  let totalAmount = monthlyPayment * countMonths;
+  const totalAmount = monthlyPayment * countMonths;
 
-  return totalAmount.toFixed(2);
+  return parseFloat(totalAmount.toFixed(2));
 }
 
+// Примеры использования
 console.log(solveEquation(1, -3, 2)); // [2, 1]
-console.log(calculateTotalMortgage(10, 10000, 50000, 12))
+console.log(solveEquation(1, 0, -4)); // [2, -2]
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12)); // 52749.53
+console.log(calculateTotalMortgage(10, 1000, 50000, 12)); // 51694.54
+console.log(calculateTotalMortgage(10, 0, 20000, 24)); // 22149.56
+console.log(calculateTotalMortgage(10, 1000, 20000, 24)); // 21042.09
+console.log(calculateTotalMortgage(10, 20000, 20000, 24)); // 0
+console.log(calculateTotalMortgage(10, 0, 10000, 36)); // 11616.19
+console.log(calculateTotalMortgage(15, 0, 10000, 36)); // 12479.52
