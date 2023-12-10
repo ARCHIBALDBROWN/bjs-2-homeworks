b909948437c2e1bfd0ce8c8268e8b36ae26064df
Archibaldbrown <bizzy_boss@mail.ru>
2023-12-10 18:30:54 +0100

    на ревью 5

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,27 +10,28 @@ let student2 = new Student("Артем", "Мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
-}
+};
 
 Student.prototype.addMarks = function (...marksToAdd) {
   if (this.marks !== undefined) {
     this.marks.push(...marksToAdd);
   }
-}
+};
 
 Student.prototype.getAverage = function () {
   let sum = 0;
-  if (this.marks === undefined || this.marks.length === 0) {
-    return null;
+  if (Array.isArray(this.marks) && this.marks.length > 0) {
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+    return sum / this.marks.length;
   }
-  for (let i = 0; i < this.marks.length; i++) {
-    sum += this.marks[i];
-  }
-  return sum / this.marks.length;
-}
+  return 0;
+};
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   this.marks = undefined;
   this.excluded = reason;
-}
+};
+
