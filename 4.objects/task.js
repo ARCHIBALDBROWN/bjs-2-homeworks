92ace54bb27d5ae5793dbc17788013a42a6dce5c
Archibaldbrown <bizzy_boss@mail.ru>
2023-12-10 13:53:52 +0100

    отправоено на ревью 6

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,47 +2,34 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
-  this.marks = []; 
+  this.marks = [];
 }
-
-let artem = new Student("Артем", "муж", 29);
-let petya = new Student("Петя", "муж", 33);
+let student1 = new Student("Василиса", "Женский", 19);
+let student2 = new Student("Артем", "Мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
-};
+}
 
-Student.prototype.addMark = function (mark) {
-  this.marks.push(mark);
-};
+Student.prototype.addMarks = function (...marksToAdd) {
+  if(this.marks !== undefined) {
+    this.marks.push(...marksToAdd);
+  }
+}
 
-Student.prototype.addMarks = function (...args) {
-  args.map(el => this.marks.push(el));
-};
-
-Student.prototype.getAverage = function () {
-  let sum = this.marks.reduce((sum, current) => sum + current);
-  this.mediumAriphetic = sum / this.marks.length;
-  return this.mediumAriphetic;
-};
+Student.prototype.getAverage = function (...marksToAdd) {
+  let sum = 0;
+  if(this.marks === undefined || this.marks.length === 0) {
+    return 0;
+  } 
+  for(let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return sum / this.marks.length;
+}
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
   this.excluded = reason;
-};
-
-artem.addMark(5);
-artem.addMark(4);
-artem.addMarks(4, 4, 3, 5, 4, 3, 4);
-artem.getAverage();
-artem.exclude("балбес");
-
-petya.addMark(2);
-petya.addMark(3);
-petya.addMarks(2, 3, 2, 2, 2, 3, 2);
-petya.getAverage();
-petya.exclude("плохая успеваемость");
-
-console.log(artem);
-console.log(petya);
+}
